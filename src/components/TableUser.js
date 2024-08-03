c8faaebfaa861130c41cb149fbdcc0603d6c3a65
Chau03 <achauboi@gmail.com>
2024-08-03 16:01:30 +0700

    Paginate

--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,24 +1,33 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { fetchAllUser } from '../services/UserService';
+import ReactPaginate from 'react-paginate';
 
 const TableUser = (props) => {
   const [listUsers, setListUser] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     //call apis
     //dry
-    getUser();
-  }, []);
+    getUser(1);
+  }, [totalPages]);
 
-  const getUser = async (user) => {
-    let res = await fetchAllUser();
+  const getUser = async (page) => {
+    let res = await fetchAllUser(page);
 
     if (res && res.data) {
+      console.log(res);
+      setTotalUsers(res.total);
       setListUser(res.data);
+      setTotalPages(res.total_pages);
     }
   };
-  console.log(listUsers);
+
+  const handlePageClick = (event) => {
+    getUser(+event.selected + 1);
+  };
 
   return (
     <>
@@ -44,6 +53,24 @@ const TableUser = (props) => {
             ))}
         </tbody>
       </Table>
+      <ReactPaginate
+        breakLabel='...'
+        nextLabel='next >'
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={4}
+        pageCount={totalPages}
+        previousLabel='< previous'
+        pageClassName='page-item'
+        pageLinkClassName='page-link'
+        previousClassName='page-item'
+        previousLinkClassName='page-link'
+        nextClassName='page-item'
+        nextLinkClassName='page-link'
+        breakClassName='page-item'
+        breakLinkClassName='page-link'
+        containerClassName='pagination'
+        activeClassName='active'
+      />
     </>
   );
 };
