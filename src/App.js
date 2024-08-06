77139a46dfc6278dd4da6dcba5cefe3b0a194a8a
Chau03 <achauboi@gmail.com>
2024-08-07 01:16:57 +0700

    design layout, route to make SPA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
 import Header from './components/Header';
+import Home from './components/Home';
 import TableUser from './components/TableUser';
 import Container from 'react-bootstrap/Container';
 import { ToastContainer } from 'react-toastify';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <div className='app-container'>
         <Header />
         <Container>
-          <TableUser />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/users' element={<TableUser />} />
+          </Routes>
         </Container>
       </div>
       <ToastContainer
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <>
+      <div className='home-container jumbotron'>
+        <div className='container'>
+          <h1 className='display-3'>Hello Guys</h1>
+          <p>This is an App for practice with reactjs</p>
+          <p>
+            This App have validate, router, fetch API from reqes.in and CRUD the
+            users
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
